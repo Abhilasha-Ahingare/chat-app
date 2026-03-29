b11d38d18701fe8a694a1298af61c4a14166c44e
abhilashaahingare <abhilashaahingare.02@gmail.com>
2026-03-29 23:18:57 +0530

    ui chnages some new added

--- Real-Time-Chat-App/clent-side/src/components/ui/multiple-selector.jsx
+++ Real-Time-Chat-App/clent-side/src/components/ui/multiple-selector.jsx
@@ -4,6 +4,7 @@ export const Option = React.memo(({ label, value, disabled }) => {
   return <div className={`option ${disabled ? "disabled" : ""}`}>{label}</div>;
 });
 
+
 const MultipleSelector = ({ defaultOptions = [], placeholder, emptyIndicator }) => {
   const [selectedOptions, setSelectedOptions] = React.useState([]);
 
