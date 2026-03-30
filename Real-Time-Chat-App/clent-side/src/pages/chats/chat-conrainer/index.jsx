05ffd9ab5c36462d86ba4fb499760174e712f31b
abhilashaahingare <abhilashaahingare.02@gmail.com>
2026-03-30 16:05:40 +0530

    code chnages

--- Real-Time-Chat-App/clent-side/src/pages/chats/chat-conrainer/index.jsx
+++ Real-Time-Chat-App/clent-side/src/pages/chats/chat-conrainer/index.jsx
@@ -2,6 +2,7 @@ import Chatheader from "./components/Chat-header";
 import MessageBar from "./components/MessageBar";
 import MessagesContainer from "./components/MessagesContainer";
 
+
 const ChatsContsiner = () => {
   return (
     <div className="flex flex-col flex-1 h-full bg-[#0d1721]">
